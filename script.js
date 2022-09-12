f34b99d410b28b202e64e307f4607b5790b0bcf4
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-09-12 11:45:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,13 @@ random answer.
 function press(){
   //asking for a space of memory
   var theirQ;
+  var rndNum;
+  //the number at the end is max value of the 
+  //random number
+  rndNum = Math.random() * 100;
+  rndNum = Math.floor(rndNum)
   //filling the variable, with whatever was in the textBox
   theirQ = document.getElementById("question").value
   document.getElementById("question").value = ""
-  document.getElementById("answer").innerHTML = theirQ
+  document.getElementById("answer").innerHTML = theirQ + rndNum
 }//end press
