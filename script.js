c7e80a57c3a256f5716d9638ebf4c047080f08a7
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-09-12 11:31:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,5 +3,10 @@ Press is going to repeat the question, and then give a
 random answer.
 */
 function press(){
-  document.getElementById("answer").innerHTML = ""
+  //asking for a space of memory
+  var theirQ;
+  //filling the variable, with whatever was in the textBox
+  theirQ = document.getElementById("question").value
+  document.getElementById("question").value = ""
+  document.getElementById("answer").innerHTML = theirQ
 }//end press
