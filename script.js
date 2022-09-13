53f367929f5bc633fe8ad76cd7907ab5c3ad0d57
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-09-13 11:17:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+//I will fill an array with responses
+var responses = [
+  "That's a hard no",
+  "Positively yes",
+  "Ask again",
+  "I am affraid not",
+  "That's a YESSSSSS!"
+]
 /*
 Press is going to repeat the question, and then give a 
 random answer.
@@ -8,10 +16,10 @@ function press(){
   var rndNum;
   //the number at the end is max value of the 
   //random number
-  rndNum = Math.random() * 7;
+  rndNum = Math.random() * responses.length;
   rndNum = Math.floor(rndNum)
   //filling the variable, with whatever was in the textBox
   theirQ = document.getElementById("question").value
   document.getElementById("question").value = ""
-  document.getElementById("answer").innerHTML = theirQ + rndNum
+  document.getElementById("answer").innerHTML = theirQ + responses[rndNum]
 }//end press
