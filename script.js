e20da1ed389edf245e46805f352ddd0ff7457b55
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-09-14 12:10:54 -0500

    Added the setTimeout and Image

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var theirQ;
+  var rndNum;
 //I will fill an array with responses
 var responses = [
   "That's a hard no",
@@ -12,8 +14,7 @@ random answer.
 */
 function press(){
   //asking for a space of memory
-  var theirQ;
-  var rndNum;
+  
   //the number at the end is max value of the 
   //random number
   rndNum = Math.random() * responses.length;
@@ -21,5 +22,12 @@ function press(){
   //filling the variable, with whatever was in the textBox
   theirQ = document.getElementById("question").value
   document.getElementById("question").value = ""
-  document.getElementById("answer").innerHTML = ">>"+theirQ+ "</br>"+responses[rndNum]
+  document.getElementById("answer").innerHTML = "Let me think about that"
+  document.getElementById("myPic").style.display = "block";
+  setTimeout(showAnswer,5000)
 }//end press
+
+function showAnswer(){
+   document.getElementById("myPic").style.display = "none";
+  document.getElementById("answer").innerHTML = ">>"+theirQ+ "</br>"+responses[rndNum]
+}//end showAnswer
